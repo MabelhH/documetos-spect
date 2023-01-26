74bd3b680766bf6804e6701eecc7cae731590175
clara23 <clarahuanaco20@gmail.com>
2023-01-26 09:11:35 -0500

    actulisacion02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spect';
+  val1:number=0;
+  val2:number=0;
+  total:number=0;
+
+operacion(){
+ if(this.seleccionada=='1'){
+  this.total=this.val1+this.val2;
+  this.sumar='green'
+ }
+ else if(this.seleccionada=='2'){
+  this.total=this.val1-this.val2;
+  this.restar='purple'
+ }
+ else if(this.seleccionada=='3'){
+  this.total=this.val1*this.val2;
+  this.multiplicar='orange'
+ }
+ else if(this.seleccionada=='4'){
+  this.total=this.val1/this.val2;
+  this.dividir='violet'
+ }
+}
+seleccionada:string="inicio";
+
+sumar:string='';
+restar:string='';
+multiplicar:string='';
+dividir:string='';
+
+
+
 }
