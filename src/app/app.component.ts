808ac71353db974058cc83990ed5e01d9353853f
clara23 <clarahuanaco20@gmail.com>
2023-01-26 11:39:30 -0500

    actulisacion05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,18 @@ export class AppComponent {
 operacion(){
  if(this.seleccionada=='1'){
   this.total=this.val1+this.val2;
-  this.sumar='green'
+
  }
  else if(this.seleccionada=='2'){
   this.total=this.val1-this.val2;
-  this.restar='purple'
+  
  }
  else if(this.seleccionada=='3'){
   this.total=this.val1*this.val2;
-  this.multiplicar='orange'
+  
  }
  else if(this.seleccionada=='4'){
   this.total=this.val1/this.val2;
-  this.dividir='violet'
  }
 }
 seleccionada:string="inicio";
